9b2979fa65a9dcf430a837332744384bd201ed82
John Guinn <jguinn124@gmail.com>
2020-04-06 16:04:32 -0500

    boilerplate stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HideableText from './HideableText.js';
+import HideableText from './AutoCompleteText.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <div>
-			<HideableText text="dynamic text"/>
-		</div>
-      </header>
+      
     </div>
   );
 }
--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -3,7 +3,23 @@ import React from 'react';
 export default class AutoCompleteText extends React.Component {
 	constructor(props){
 		super(props);
+		this.items = [
+			'St. Louis',
+			'Columbia',
+			'Kansas City'
+		];
+		}
 
+
+		render () {
+			return (
+			<div>
+				<input type="text" />
+				<ul>
+					{this.items.map((item) => <li>{item} </li> )}
+				</ul>
+			</div>
+			)
 		}
 	}
 
